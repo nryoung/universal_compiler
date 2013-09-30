62d8f1752a7165ad87d3fd21022a29b759d6825f
Nic Young <nryoung@gmail.com>
2013-09-29 21:40:39 -0600

    Grammar analyzer now lists all non-terminal symbols.

--- compiler/grammar_analyzer.py
+++ compiler/grammar_analyzer.py
@@ -18,16 +18,39 @@ class GrammarAnalyzer(object):
         return symbols
 
 
+    def get_non_terminals(self):
+        g = self.productions
+        nt_list = []
+        non_terminals = set()
 
+        for prod in g:
+            nt = ''
+            for c in prod:
+                if c == ' ':
+                    break
+                else:
+                    nt += c
+            nt_list.append(nt)
+        non_terminals.update(nt_list)
+
+        return non_terminals
 
 
     def analyze(self):
         symbols = self.get_symbols()
+        non_terminals = self.get_non_terminals()
 
         print "productions:"
+        print "------------"
         for prod in self.productions:
             print prod
 
         print "symbols:"
+        print "--------"
         for sym in symbols:
             print sym
+
+        print "non terminals:"
+        print "--------------"
+        for nt in non_terminals:
+            print nt
