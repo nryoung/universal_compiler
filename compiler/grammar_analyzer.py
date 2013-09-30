67e070524c123493a9b9b5655a9b839111f60a43
Nic Young <nryoung@gmail.com>
2013-09-29 21:22:50 -0600

    Added whitespace and underscore in grammar. GrammarAnalyzer now allows retrieves set of symbols from the grammar.

--- compiler/grammar_analyzer.py
+++ compiler/grammar_analyzer.py
@@ -7,7 +7,27 @@ class GrammarAnalyzer(object):
     def __init__(self, grammar):
         self.productions = grammar.readlines()
 
+    def get_symbols(self):
+        g = self.productions
+        symbols = set()
+
+        for prod in g:
+            sym = prod.split()
+            sym.remove('->')
+            symbols.update(sym)
+        return symbols
+
+
+
+
+
     def analyze(self):
+        symbols = self.get_symbols()
+
         print "productions:"
         for prod in self.productions:
             print prod
+
+        print "symbols:"
+        for sym in symbols:
+            print sym
