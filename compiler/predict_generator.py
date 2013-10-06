ecbe49672d39ead93bb5f1a20298237a6dd50203
Nic Young <nryoung@gmail.com>
2013-10-06 01:31:40 -0600

    Predict generator mark_lambda now working.

--- compiler/predict_generator.py
+++ compiler/predict_generator.py
@@ -1,11 +1,42 @@
 """
 Predict Generator implementation.
 """
+from .grammar_analyzer import GrammarAnalyzer
 
 class PredictGenerator(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, prods):
+        self.ga = GrammarAnalyzer(prods)
+        self.derives_lambda = {}
 
-    def mark_lambda(g):
-        pass
+    def mark_lambda(self):
+
+        changes = True
+        rhs_derives_lambda = None
+
+        # construct this the crappy way at first
+        for s in self.ga.symbols:
+            self.derives_lambda[s] = False
+
+        # explicitly mark lambda as True
+        self.derives_lambda['lambda'] = True
+
+        while changes:
+            print self.derives_lambda
+            changes = False
+            for p in self.ga.productions:
+                rhs_derives_lambda = True
+                lhs = self.ga.get_lhs(p)
+                rhs = self.ga.get_rhs(p)
+
+                for i in rhs:
+                    rhs_derives_lambda = rhs_derives_lambda and self.derives_lambda[i]
+
+                if rhs_derives_lambda and not self.derives_lambda[lhs]:
+                    changes = True
+                    self.derives_lambda[lhs] = True
+
+
+    def generate(self):
+        self.mark_lambda()
+        print self.derives_lambda
