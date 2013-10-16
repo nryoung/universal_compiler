bd7fa97e4fb9001713f283349ff9ab4a67ae2b2f
Nic Young <nryoung@gmail.com>
2013-10-15 23:28:41 -0600

    Initial commit of table_gen method. Does not handle multiple entries yet.

--- compiler/predict_generator.py
+++ compiler/predict_generator.py
@@ -11,6 +11,7 @@ class PredictGenerator(object):
         self.first_sets = {}
         self.follow_sets = {}
         self.predict_sets = {}
+        self.predict_tbl = [[' ']]
 
     def mark_lambda(self):
 
@@ -130,6 +131,53 @@ class PredictGenerator(object):
                 self.predict_sets[rhs].update(self.follow_sets[rhs])
             else:
                 self.predict_sets[rhs].update(temp)
+
+    def _gen_predict_table(self):
+        # first create our columns
+        self.predict_tbl[0].extend([t for t in self.ga.get_terminals() if t != 'lambda'])
+        col_matching = {}
+        i = 0
+        # ugh, iterate through to get column number for corresponding symbols
+        for t in self.predict_tbl[0]:
+            col_matching[t] = i
+            i += 1
+
+        # we then iterate through our productions
+        p_num = 0
+        for p in self.ga.productions:
+            p_num += 1
+
+            # first we create the row and update our table
+            row = [' '] * len(self.predict_tbl[0])
+            lhs = self.ga.get_lhs(p)
+            row[0] = lhs
+            self.predict_tbl.extend([row])
+
+            # now we match cols and update
+            # iterate through first_sets and look for matches
+            tks = [x for x in self.first_sets[lhs]]
+
+            # test for lambda, if present we need to add predict set
+            if 'lambda' in tks:
+                tks.remove('lambda')
+                tks.extend([ x for x in self.follow_sets[lhs]])
+
+            for t in tks:
+                self.predict_tbl[-1][col_matching[t]] = p_num
+
+
+
+
+    def generate_predict_table(self, start_sym):
+        # first call of the methods we need to fill our sets
+        self.mark_lambda()
+        self.fill_first_set()
+        self.fill_follow_set(start_sym)
+        self.fill_predict_set()
+
+        # now generate our predict table using our predict set
+        self._gen_predict_table()
+
 
     def generate(self, start_sym):
         self.mark_lambda()
