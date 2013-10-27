81e58ea6eb26f29754f28879bcdec9da8237816d
Nic Young <nryoung@gmail.com>
2013-10-26 22:50:23 -0600

    Imports of scanner and predict_generator.

--- compiler/parser.py
+++ compiler/parser.py
@@ -1,8 +1,11 @@
 """
 Parser implementation.
 """
-
+from .scanner import Scanner
+from .predict_generator import PredictGenerator
 
 class Parser(object):
-    def __init__(self):
-        pass
+
+    def __init__(self, program, grammar, start_sym):
+        self.scanner = Scanner(program)
+        self.pg = PredictGenerator(grammar)
