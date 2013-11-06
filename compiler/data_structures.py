8eae399c9af46ca422da4bcb86eab3190d3304ec
Nic Young <nryoung@gmail.com>
2013-11-05 22:46:19 -0700

    Initial commit of data structures to be used by compiler.

---
+++ compiler/data_structures.py
@@ -0,0 +1,59 @@
+"""
+Data structures to be used in the universal compiler.
+"""
+
+from compiler_errors import OpError
+from compiler_errors import LiteralError
+from compiler_errors import IdError
+
+class OpRec(object):
+
+    allowed_vals = ['-', '+', None]
+
+    def __init__(self, operator=None):
+        self.op = operator
+
+    @property
+    def op(self):
+        return self._op
+
+    @op.setter
+    def op(self, o):
+        if o not in self.allowed_vals:
+            raise OpError("Operator must either be a '+' or '-'")
+        else:
+            self._op = o
+
+class ExprRec(object):
+
+    def __init__(self, expr=None, literal=None):
+        if literal:
+            self.val = expr
+        else:
+            self.name = expr
+
+    @property
+    def val(self):
+        return self._val
+
+    @val.setter
+    def val(self, expr):
+        try:
+            v = int(expr)
+        except ValueError:
+            raise LiteralError("Invalid invalid literal with base 10: '%s'" % expr)
+        else:
+            self._val = v
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, expr):
+        try:
+            n = int(expr)
+        except (ValueError, TypeError):
+            self._name = expr
+        else:
+            raise IdError("Invalid Id name: '%s'" % expr)
