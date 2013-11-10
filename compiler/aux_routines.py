e84a93f8643a4a484e7968882500b03bc1d7eb48
Nic Young <nryoung@gmail.com>
2013-11-10 11:03:54 -0700

    Initial commit of aux and sem routines.

---
+++ compiler/aux_routines.py
@@ -0,0 +1,57 @@
+"""
+Auxiliary Routines for the universal compiler.
+"""
+import sys
+
+# Globals
+max_temp = 0
+
+def generate(s1, s2=None, s3=None, s4=None):
+    if s4:
+        sys.stdout.write("%s %s, %s, %s\n" % (s1, s2, s3, s4))
+    elif s3:
+        sys.stdout.write("%s %s, %s\n" % (s1, s2, s3))
+    else:
+        sys.stdout.write("%s" % s1)
+
+def extract_sem(sem_rec):
+    if sem_rec is ExprRec:
+        return extract(sem_rec)
+    elif sem_rec is OpRec:
+        return extract_op(sem_rec)
+    else:
+        raise CompilerError
+
+def extract(e):
+    if e.name:
+        return str(e.name)
+    else:
+        return int(e.val)
+
+def extract_op(o):
+    if o.op == "+":
+        return "ADD"
+    else:
+        return "SUB"
+
+def look_up(s, symbol_table):
+    if s in symbol_table:
+        return  True
+    else:
+        return False
+
+def enter(s, symbol_table):
+    symbol_table.append(s)
+
+def check_id(s, symbol_table):
+    if not look_up(s, symbol_table):
+        enter(s, symbol_table)
+        generate("DECLARE", s, "Integer")
+
+def get_temp(symbol_table):
+    global max_temp
+    max_temp += 1
+
+    temp_name = "Temp&%s" % max_temp
+    check_id(temp_name, symbol_table)
+    return temp_name
---
+++ compiler/sem_routines.py
@@ -0,0 +1,43 @@
+"""
+Semantic routines for the universal compiler.
+"""
+from aux_routines import generate
+from aux_routines import extract
+from aux_routines import extract_op
+from aux_routines import get_temp
+from aux_routines import check_id
+from data_structures import ExprRec
+
+def start(symbol_table):
+    symbol_table[:] = []
+
+def assign(target, source):
+    generate("STORE", extract_sem(source), target.name)
+
+def read_id(invar):
+    generate("READ", invar.expr_rec.name, "Integer")
+
+def write_expr(outexpr):
+    generate("WRITE", extract_sem(outexpr), "Integer")
+
+def gen_infix(e1, op, e2, symbol_table):
+    temp_expr = ExprRec(get_temp(symbol_table))
+    generate(extract_sem(op), extract_sem(e1), extract_sem(e2), temp_expr.expr_rec.name)
+    return temp_expr
+
+def process_id(e, token_buffer, symbol_table):
+    ### See how this is used before editing
+    check_id(token_buffer, symbol_table)
+    e.name = token_buffer
+
+def proc_literal(e, token_buffer):
+    ### See how this is used before editing
+    # mocking token buffer here too
+    e.val = int(token_buffer)
+
+def process_op(o, current_token):
+    # mocking current token
+    o.op = current_token
+
+def finish():
+    generate("HALT")
