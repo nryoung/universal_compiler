0c101d221bb8ce74a700d98318df2bc78d55d6fc
Nic Young <nryoung@gmail.com>
2013-11-16 12:21:19 -0700

    Removed ll_driver method to avoid confusion. Added _call_action method to call related aux and sem routines.

--- compiler/parser.py
+++ compiler/parser.py
@@ -4,6 +4,7 @@ Parser implementation.
 from .scanner import Scanner
 from .predict_generator import PredictGenerator
 from .compiler_errors import SyntaxError
+import sem_routines
 
 class Parser(object):
 
@@ -55,56 +56,10 @@ class Parser(object):
         print "(%s, %s, %s, %s)" % (idx[0], idx[1], idx[2], idx[3])
         print '-' * 80
 
-    def ll_driver(self):
-        self.stack.append(self.start_sym)
-        a = self.scanner.scan()
+    def _call_action(self, X):
+        action = X.split('#')[1].lower()
+        print "action would be: sem_routines." + action
 
-        while self.stack:
-            X = self.stack[-1]
-
-            # Unfortunately the predict table does not account for empty space
-            if a == 'EmptySpace' or a == 'Comment':
-                a = self.scanner.scan()
-                continue
-
-            # Doesn't account for lambda either? hmmm
-            if X == 'lambda':
-                self.stack.pop()
-                continue
-
-            if X in self.pg.ga.get_non_terminals():
-                # from the token yielded from scanner we need to map to correct col
-                # thus we do this by do calls to correct dicts
-                col = self.rev_tk_mapping[a]
-                col_num = self.pg.col_matching[col]
-
-                # Now we have to iterate through the table to find the right row.
-                # perhaps a func can be made in pg to do this?
-                new_X = None
-                predict_num = None
-                for row in self.pg.predict_tbl:
-                    if X == row[0]:
-                        if row[col_num] != ' ':
-                            predict_num = row[col_num]
-                            new_X = self.pg.ga.productions[row[col_num] - 1]
-                            new_X = self.pg.ga.get_rhs(new_X)
-                        else:
-                            raise SyntaxError(a)
-
-                self._display_row(a, pn=predict_num)
-                # pop the items of the stack and replace them with new stuff
-                self.stack.pop()
-                for x in new_X[::-1]:
-                    self.stack.append(x)
-
-
-            elif X in self.pg.ga.get_terminals():
-                if X == self.rev_tk_mapping[a]:
-                    self._display_row(a)
-                    self.stack.pop()
-                    a = self.scanner.scan()
-                else:
-                    raise SyntaxError(a)
 
     def ll_compiler(self):
         token_text, a = self.scanner.scan()
@@ -199,4 +154,4 @@ class Parser(object):
             else:
                 print "X is action symbol: %s" % X
                 self.stack.pop()
-                # call semantic record here, somehow.
+                self._call_action(X)
